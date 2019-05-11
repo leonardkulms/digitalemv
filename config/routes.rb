4923cfb4b40861c3e31d611d470b5830e898a6f6
Leonard Kulms <kulmsleonard@gmail.com>
2019-05-11 23:37:39 +0200

    users, meetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   resources :meetings
+  root 'meetings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
